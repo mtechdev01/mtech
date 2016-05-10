19cd410d2b76deac278a4a7c0ccc487cd9f51cef
Jemoms <morej03@gmail.com>
2016-05-10 17:07:32 +0200

    validations models

--- app/models/project.rb
+++ app/models/project.rb
@@ -11,6 +11,12 @@ class Project < ActiveRecord::Base
 
   scope :published, -> { where(published: true) }
 
+  validates :name, presence: { message: "Le nom de votre projet est obligatoire" }, uniqueness: { message: "Ce nom a déjà été utilisé" }, length: { maximum: 120, too_long: "Le nom du projet ne doit pas dépasser les %{count} caractères" }
+  validates :content, presence: { message: "Le contenu de votre projet est obligatoire" }
+  validates :location, presence: { message: "Le lieu est nécessaire"}
+  validates_associated :category
+  validates_associated :owner
+
   def self.lastProjects user
     where( author_id: user.id ).order( created_at: :desc)
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :confirmable, :validatable
   devise :omniauthable, :omniauth_providers => [:facebook, :google_oauth2, :twitter]
@@ -9,6 +9,9 @@ class User < ActiveRecord::Base
   has_many :comments
   has_many :surveysResponses, class_name: "SurveysResponse"
   has_many :likes
+
+  validates_presence_of :city, :cp, :username, :email, :country, :territory_attachment, :first_name, :last_name, :message => "Ce champ est obligatoire"
+  validates_uniqueness_of :email, :username, :message => "Déjà utilisé"
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
