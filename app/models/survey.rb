54427cb639ce9a2a120612ff22656c7a9f197c5a
Jemoms <morej03@gmail.com>
2016-06-10 17:34:16 +0200

    upload pdf

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -3,6 +3,8 @@ class Survey < ActiveRecord::Base
   has_many :responses, class_name: 'SurveysResponse', foreign_key: "survey_id", dependent: :delete_all
   accepts_nested_attributes_for :fields
 
+  mount_uploader :pdf_file, PdfsondageUploader
+
   validates :title, presence: { message: "Le titre du sondage est obligatoire" }, uniqueness: { message: "Ce titre a déjà été utilisé" }, length: { maximum: 120, too_long: "Le titre ne doit pas dépasser les %{count} caractères" }
   validates :description, presence: { message: "La description du sondage est obligatoire" }, length: { maximum: 350, too_long: "La description doit être concise" }
 
