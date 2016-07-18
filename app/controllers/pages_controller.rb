3729bff1afc94b6b09de57de2f661cfdee982c7c
Jemoms <morej03@gmail.com>
2016-07-18 16:59:04 +0200

    fix last_projects + début index articles

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,7 @@ class PagesController < ApplicationController
 
   def home
     @most_active_projects = Project.mostActive
-    @last_projects = Project.lasts(6)
+    @last_projects = Project.lasts(3)
   end
 
   def about
