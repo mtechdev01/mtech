8d43c3fe67971471a431638cd8f236046764f17c
Vagrant User <vagrant@local2.dev>
2016-07-18 08:50:48 +0000

    Insert maquette - home+partials

---
+++ app/assets/javascripts/home.js
@@ -0,0 +1,23 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require libs/owl.carousel.min.js
+
+$('.owl-carousel').owlCarousel({
+  loop:true,
+  margin:0,
+  nav:true,
+  dots:false,
+  items:1,
+  autoplay:true,
+  navText: ['<i class="fa fa-caret-left"></i>','<i class="fa fa-caret-right"></i>']
+})
